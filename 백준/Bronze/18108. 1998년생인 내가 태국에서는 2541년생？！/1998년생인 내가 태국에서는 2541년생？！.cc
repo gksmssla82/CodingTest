12a26f28ebc6d79f6c6b8e99bec188c3337f0543
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-01-08 04:00:16 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int iYears, iBulgiYears;
+
+	cin >> iBulgiYears;
+
+	iYears = iBulgiYears;
+
+	iYears -= 543;
+
+	cout << iYears << endl;
+
+	
+}
