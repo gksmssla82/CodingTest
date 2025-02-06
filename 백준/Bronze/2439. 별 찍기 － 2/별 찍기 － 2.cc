74630ed44fe9414c75952b9c1f207aef74bbf89d
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-02-06 17:20:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -1,31 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-
-// 공백 4,3,2,1 , 별 1 2 3 4 5
 int main()
 {
-	int count = 0;
-	
-	cin >> count;
+    int n;
 
-	for (int i = 0; i < count; ++i)
-	{
-		for (int j = 0; j < count - 1 - i; ++j)
-		{
-			cout << " ";
-		}
-		
-		for (int p = 0; p < i + 1; ++p)
-		{
-			cout << "*";
-		}
+    cin >> n;
 
-		cout << endl;
-	
-	}
+    for (int i = 0; i < n; ++i)
+    {
+        // n = 5   value = 4, 3, 2, 1
+        for (int j = 0; j < n - 1 - i; ++j)
+            cout << ' ';
+
+        for (int t = 0; t < i + 1; ++t)
+            cout << '*';
 
 
-	return 0;
+        cout << '\n';
+    }
+
+    return 0;
 }
