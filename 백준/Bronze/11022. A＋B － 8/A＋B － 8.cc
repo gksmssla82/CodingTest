50702019180ccfad90d4e919e757d3e4a40b9cca
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-02-07 10:11:22 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    int a, b;
+
+    cin >> t;
+
+    for (int i = 1; i < t + 1; ++i)
+    {
+        cin >> a >> b;
+
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
+    }
+
+    return 0;
+}
