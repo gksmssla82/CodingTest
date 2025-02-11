200af5266e5daf46aed4618e486038f3997fe2f8
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-02-11 15:48:28 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+			cout << ' ';
+
+		for (int p = 0; p < n - i; ++p)
+			cout << '*';
+
+		cout << '\n';
+	}
+
+	return 0;
+}
