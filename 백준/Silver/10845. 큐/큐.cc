f26bbb3db5f0507520cf2885e34f4ae11d5c637b
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-02-19 01:55:39 +0900

    [Silver IV] Title: 큐, Time: 220 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	string op;
+
+	queue<int> q;
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> op;
+
+		if (op == "push")
+		{
+			int add;
+			cin >> add;
+
+			q.emplace(add);
+		}
+
+		else if (op == "pop")
+		{
+			if (q.empty())
+				cout << -1 << '\n';
+			else
+			{
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+
+		else if (op == "size")
+			cout << q.size() << '\n';
+
+		else if (op == "empty")
+			q.empty() ? cout << 1 << '\n' : cout << 0 << '\n';
+
+		else if (op == "front")
+			q.empty() ? cout << -1 << '\n' : cout << q.front() << '\n';
+
+		else if (op == "back")
+			q.empty() ? cout << -1 << '\n' : cout << q.back() << '\n';
+	}
+
+	return 0;
+}
