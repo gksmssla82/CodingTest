1abd1c0a327ad8b492568c25e16b0ef74d2241ff
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-02-28 21:05:39 +0900

    [Gold V] Title: 토마토, Time: 216 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -9,10 +9,6 @@
 
 using namespace std;
 
-// Input 1 = 익은 토마토, 0 = 익지 않은 토마토, -1 토마토가 없는 위치
-// Output 0 = 처음부터 모두 토마토가 익어있음, -1 토마토가 모두 익지 못하는 상황, else = 토마토가 모두 익는 일수
-// 익은토마토가 처음에 여러개일수도있음
-
 int board[1002][1002];
 int check[1002][1002];
 
@@ -20,7 +16,6 @@ int main()
 {
 	int n, m;
 	queue<pair<int, int>> q;
-
 	int nx[4] = { 1,0,-1,0 };
 	int my[4] = { 0,1,0,-1 };
 
@@ -32,16 +27,18 @@ int main()
 		{
 			cin >> board[i][j];
 
-			if (board[i][j] == 1)
-				q.emplace(i, j);
 			if (board[i][j] == 0)
 				check[i][j] = -1;
+			if (board[i][j] == 1)
+				q.emplace(i, j);
 		}
 	}
 
+	
+
 	while (!q.empty())
 	{
-		pair<int, int> cur = q.front(); q.pop();
+		auto cur = q.front(); q.pop();
 
 		for (int i = 0; i < 4; ++i)
 		{
@@ -58,7 +55,7 @@ int main()
 		}
 	}
 
-	int ans = 0;
+	int day = 0;
 
 	for (int i = 0; i < n; ++i)
 	{
@@ -69,12 +66,11 @@ int main()
 				cout << -1;
 				return 0;
 			}
-
-			ans = max(ans, check[i][j]);
+			day = max(day, check[i][j]);
 		}
 	}
 
-	cout << ans;
+	cout << day;
 
 	return 0;
 }
