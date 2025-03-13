7b41c55f74cfa9db09765eb0309942b8bbe1a5a5
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-03-13 21:31:43 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -2,33 +2,28 @@
 
 using namespace std;
 
-int func(int n, int r, int c)
+int n, r, c;
+
+int recursion(int n, int r, int c)
 {
 	if (n == 0)
 		return 0;
 
-	// 한변 길이의 절반
 	int half = 1 << (n - 1);
 
-	// 1번사각형
 	if (r < half && c < half)
-	return func(n - 1, r, c);
-	// 2번사각형
+		return recursion(n - 1, r, c);
 	if (r < half && c >= half)
-	return half * half + func(n - 1, r, c - half);
-	// 3번사각형
+		return half * half + recursion(n - 1, r, c - half);
 	if (r >= half && c < half)
-	return 2 * half * half + func(n - 1, r - half, c);
-	// 4번사각형
-	return 3 * half * half + func(n - 1, r - half, c - half);
+		return 2 * half * half + recursion(n - 1, r - half, c);
+
+	return 3 * half * half + recursion(n - 1, r - half, c - half);
 }
 
 int main()
 {
-	int n, r, c;
-
 	cin >> n >> r >> c;
 
-	cout << func(n, r, c);
-
+	cout << recursion(n, r, c);
 }
