d6dea55b2436377b62ef77f7a8b1d595f662932b
ShinSeungHyun <97272988+gksmssla82@users.noreply.github.com>
2025-10-27 16:12:01 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,67 +1,72 @@
 #include <iostream>
-#include <algorithm>
-#include <list>
 #include <vector>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
 
 using namespace std;
 
-int board[502][502];
-bool check[502][502];
+int n, m;
+int map[505][505];
+bool vis[505][505];
+
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+
+int cnt = 0;
+int mxToken = 0;
+
+int dfs(int y, int x)
+{
+	vis[y][x] = true;
+
+	int token = 1;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (0 > ny || 0 > nx || ny >= n || nx >= m) continue;
+		if (vis[ny][nx] || map[ny][nx] == 0) continue;
+
+		token += dfs(ny, nx);
+	}
+
+	return token;
+}
 
 int main()
 {
-	int n, m;
-	int num = 0, width = 0;
-	queue<pair<int, int>> q;
-	int nx[4] = { 1,0,-1,0 };
-	int my[4] = { 0,1,0,-1 };
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
 	cin >> n >> m;
-
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			cin >> board[i][j];
 
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
 		{
-			if (check[i][j] || board[i][j] != 1)
-				continue;
-
-			check[i][j] = true;
-			q.emplace(i, j);
-			++num;
-			int area = 0;
-
-			while (!q.empty())
-			{
-				auto cur = q.front(); q.pop();
-				++area;
-
-				for (int i = 0; i < 4; ++i)
-				{
-					int x = cur.first + nx[i];
-					int y = cur.second + my[i];
-
-					if (x < 0 || x >= n || y < 0 || y >= m)
-						continue;
-					if (check[x][y] || board[x][y] != 1)
-						continue;
-
-					check[x][y] = true;
-					q.emplace(x, y);
-				}
-			}
-
-			width = max(width, area);
+			cin >> map[i][j];
 		}
 	}
 
-	cout << num << '\n' << width;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			if (!vis[i][j] && map[i][j] == 1)
+			{
+				++cnt;
+				
+				mxToken = max(mxToken, dfs(i, j));
+			}
+		}
+	}
+
+	cout << cnt << '\n';
+	cout << mxToken;
+	
+	
+
 	return 0;
 }
